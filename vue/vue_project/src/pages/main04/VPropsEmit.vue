b4e0897d5a707069058d2674df99a9b1b287a866
js.yoon <js.yoon@adcapsule.co.kr>
2024-08-27 13:46:09 +0900

    JS - props 와 emit 관련 파일 및 문제 업로드

---
+++ vue/vue_project/src/pages/main04/VPropsEmit.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Day 04 - props (부모 -> 자식) && emit (자식 -> 부모) 익히기 <br> <sub>vue3 기준</sub></h2>
+        <p>props 를 이용 하여 'VPropsEmit.vue' 데이터 [김은지] 를 넘기고 자식 컴포넌트에서 emit를 이용하여 [윤지선] 을 출력되게 해주세요. </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    //** components 지역등록**
+    components: {
+    },
+    data(){
+        return{
+
+        };
+    },
+};
+</script>
